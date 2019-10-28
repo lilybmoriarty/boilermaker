f7bcb49880187d6e0e91bc0e509d83db1111a48a
Lillian Moriarty <lilybmoriarty@gmail.com>
2019-10-28 11:57:07 -0400

    finished react / redux sections

---
+++ client/reducers/index.js
@@ -0,0 +1,13 @@
+// include actions
+
+// include action creators
+
+// include thunks
+
+// set initial state
+const initialState = {};
+
+//reducers
+export default function dummyReducer(state = initialState, action) {
+  return state;
+}
